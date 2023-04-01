7c9bc36c2aa0e92dcb9823150cd6ee9d1c47d9c2
Jonathan Sum <777JonathanSum@gmail.com>
2023-04-01 03:48:32 -0700

    update
    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </header>
+  );
+}
+
+export default App;
