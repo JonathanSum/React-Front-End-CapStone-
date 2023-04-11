e9b1aa6024f6403a8fb4a24d5b8a3f888ab31679
Jonathan Sum <777JonathanSum@gmail.com>
2023-04-10 20:50:48 -0700

    update
    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import BookingForm from "./view/BookingForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders the BookingForm heading", () => {
+  // render the App component
+  render(<BookingForm />);
+  const headingElement = screen.getByText("Book Now");
+
+  expect(headingElement).toBeInTheDocument();
 });
--- src/view/BookingForm.js
+++ src/view/BookingForm.js
@@ -29,66 +29,69 @@ const BookingForm = ({ dispatch }) => {
     // console.log("occassion: " + occassion);
   }, [date, time, guests, occassion]);
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
-    >
-      <label htmlFor="res-date">Choose date</label>
-      <input
-        type="date"
-        id="res-date"
-        value={date}
-        onChange={(e) => {
-          setDate(e.target.value);
-          console.log("typeof: " + dispatch);
-          dispatch({ type: "date", value: e.target.value });
-        }}
-      ></input>
+    <>
+      <h1 data-test-id="BookNow">{"Book Now"}</h1> 
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+      >
+        <label htmlFor="res-date">Choose date</label>
+        <input
+          type="date"
+          id="res-date"
+          value={date}
+          onChange={(e) => {
+            setDate(e.target.value);
+            console.log("typeof: " + dispatch);
+            dispatch({ type: "date", value: e.target.value });
+          }}
+        ></input>
 
-      <label htmlFor="res-time">Choose time</label>
-      <select
-        id="res-time"
-        value={time}
-        onChange={(e) => {
-          setTime(e.target.value);
-          dispatch({ type: "time", value: e.target.value });
-        }}
-      >
-        <option value="17:00">17:00</option>
-        <option value="18:00">18:00</option>
-        <option value="19:00">19:00</option>
-        <option value="20:00">20:00</option>
-        <option value="21:00">21:00</option>
-        <option value="22:00">22:00</option>
-      </select>
-      <label htmlFor="guests">Number of guests</label>
-      <input
-        type="number"
-        placeholder="1"
-        min="1"
-        max="10"
-        id="guests"
-        value={guests}
-        onChange={(e) => {
-          setGuests(e.target.value);
-          dispatch({ type: "guests", value: e.target.value });
-        }}
-      />
-      <label htmlFor="occasion">Occassion</label>
-      <select
-        name="occasion"
-        id="occasion"
-        value={occassion}
-        onChange={(e) => {
-          setOccassion(e.target.value);
-          dispatch({ type: "occasion", value: e.target.value });
-        }}
-      >
-        <option value="Birthday">Birthday</option>
-        <option value="Anniversary">Anniversary</option>
-      </select>
-      <input type="submit" value="Make Your reservation" />
-    </form>
+        <label htmlFor="res-time">Choose time</label>
+        <select
+          id="res-time"
+          value={time}
+          onChange={(e) => {
+            setTime(e.target.value);
+            dispatch({ type: "time", value: e.target.value });
+          }}
+        >
+          <option value="17:00">17:00</option>
+          <option value="18:00">18:00</option>
+          <option value="19:00">19:00</option>
+          <option value="20:00">20:00</option>
+          <option value="21:00">21:00</option>
+          <option value="22:00">22:00</option>
+        </select>
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          value={guests}
+          onChange={(e) => {
+            setGuests(e.target.value);
+            dispatch({ type: "guests", value: e.target.value });
+          }}
+        />
+        <label htmlFor="occasion">Occassion</label>
+        <select
+          name="occasion"
+          id="occasion"
+          value={occassion}
+          onChange={(e) => {
+            setOccassion(e.target.value);
+            dispatch({ type: "occasion", value: e.target.value });
+          }}
+        >
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+        </select>
+        <input type="submit" value="Make Your reservation" />
+      </form>
+    </>
   );
 };
 
